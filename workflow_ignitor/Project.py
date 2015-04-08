8376c64c113fdb93b671152d534e42f70d1474e4
m.lewandowski <m.lewandowski@cksource.com>
2015-04-08 23:54:04 +0200

    Added custom properties handling to the Project class.

--- workflow_ignitor/Project.py
+++ workflow_ignitor/Project.py
@@ -8,7 +8,7 @@ class Project:
 	Currently we're strictly focused on project from a programmer POV, so it needs to be located within certian directory.
 	'''
 	
-	def __init__( self, projectId, path ):
+	def __init__( self, projectId, path, properties = None ):
 		'''
 		Constructor for Project.
 		
@@ -16,6 +16,7 @@ class Project:
 		'''
 		self.id = projectId
 		self.setPath( path )
+		self._properties = properties or {}
 	
 	def setPath( self, path ):
 		'''
@@ -26,4 +27,39 @@ class Project:
 			self.path = path
 		else:
 			raise IOError( 'Project directory "{0}" not found.'.format( path ) )
+	
+	def setProperty( self, name, value ):
+		'''
+		Sets custom property to a given value.
+		'''
+		if not isinstance( name, str ):
+			raise TypeError( 'Invalid type given for name. Expected {0}, while str was expected.'.format( type( name ) )  )
+		
+		parts = str( name ).split( '.' )
+		
+		curScope = self._properties
+		
+		for curPart in parts[ 0 : -1 ]:
+			if not curPart in curScope.keys():
+				curScope[ curPart ] = {}
+			
+			curScope = curScope[ curPart ]
+		
+		curScope[ parts[ -1 ] ] = value
+	
+	def getProperty( self, name ):
+		
+		parts = str( name ).split( '.' )
+		
+		curScope = self._properties
+		
+		for curPart in parts[ 0 : -1 ]:
+			if not curPart in curScope.keys():
+				return None
+			else:
+				curScope = curScope[ curPart ]
+		
+		return curScope[ parts[ -1 ] ]
+	
+	
 	
