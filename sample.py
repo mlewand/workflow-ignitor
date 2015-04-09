95126808436a6029fad9126180ff289b7ce93355
m.lewandowski <m.lewandowski@cksource.com>
2015-04-09 09:09:21 +0200

    Fixed #1 - Updated sample code.

--- sample.py
+++ sample.py
@@ -1,17 +1,24 @@
 
-import os
-
+# This sample will create an issue in your current project with a fixed, junk content.
 from workflow_ignitor.WorkflowIgnitor import WorkflowIgnitor
 from workflow_ignitor.github.GitHubIntegration import GitHubIntegration
 
 app = WorkflowIgnitor()
+# Include GitHub integration.
 app.registerIntegration( GitHubIntegration )
 
+# Issue source text. First line is a title, and all the rest is the description.
 issueText = '''Lorem ipsum
 
 This is test issue.
 '''
 
-ret = app.issues.reportIssueFromText( issueText )
+print( 'This script will add junk issue to your current project. Type "y" to continue.' )
+resp = input( '' )
 
-print( 'done' )
+if resp != 'y':
+	print( 'Typed "{}", aborting.'.format( resp ) )
+	exit()
+
+# Reports the issue.
+app.issues.reportIssueFromText( issueText )
