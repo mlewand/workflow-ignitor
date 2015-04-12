34ad4324fc74e9014807011ab7d9bed2993619b2
m.lewandowski <m.lewandowski@cksource.com>
2015-04-12 02:58:14 +0200

    Fixed #4 - Implemented issue create action to the IssueController.

--- tests/controller/testIssueController.py
+++ tests/controller/testIssueController.py
@@ -1,5 +1,7 @@
 
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
+from io import StringIO
+import sys, os
 
 from tests.BaseTestCase import BaseTestCase
 from workflow_ignitor.controller.IssueController import IssueController
@@ -22,3 +24,78 @@ class testIssueController( BaseTestCase ):
 		
 		textParserMock.parse.assert_called_once_with( srcText )
 		self.mock._reportIssue.assert_called_once_with( issueMock, self.owner.getProject() )
+	
+	@patch( '__main__.sys.stdin', StringIO( 'foo\n\nbar' ) )
+	def testActionCreateStdin( self ):
+		'''
+		Ensure that stdin input is taken by default.
+		'''
+		
+		mock = Mock()
+		args = Mock()
+		args.file = None
+		IssueController.actionCreate( mock, args )
+		
+		mock.reportIssueFromText.assert_called_once_with( 'foo\n\nbar' )
+	
+	@patch( '__main__.sys.stdin', StringIO( '' ) )
+	def testActionCreateStdinEmpty( self ):
+		mock = Mock()
+		args = Mock()
+		args.file = None
+		self.assertRaisesRegex( RuntimeError, '^Empty buffer given to stdin. You\'re supposed to provide issue content with stdin\.$', IssueController.actionCreate, mock, args )
+	
+	@patch( '__main__.sys.stdin', StringIO( ' \t\n\t ' ) )
+	def testActionCreateStdinWhitespace( self ):
+		mock = Mock()
+		args = Mock()
+		args.file = None
+		self.assertRaisesRegex( RuntimeError, '^Empty buffer given to stdin. You\'re supposed to provide issue content with stdin\.$', IssueController.actionCreate, mock, args )
+
+	@patch( '__main__.sys.stdin' )
+	def testActionCreateFromFile( self, stdinMock ):
+		'''
+		If --file arg is present then we should use file instead stdin.
+		
+		Note: I wasn't able to successfuly mock __main__.open method. Though it's well docummented: https://docs.python.org/3.3/library/unittest.mock.html#mock-open
+		
+		This is why I'm going to use real fixture files.
+		
+		It seems not to work here: Win7 Python 3.4.0 x64
+		
+		example code:
+		
+		>>>	with patch('__main__.open', mock_open(read_data='bibble'), create=True) as m:
+		>>>		with open('foo') as h:
+		>>>			result = h.read()
+		Traceback (most recent call last):
+			File "X:\dev\workflow-ignitor\tests\controller\testIssueController.py", line 74, in testActionCreateFromFile
+				with open('foo') as h:
+		FileNotFoundError: [Errno 2] No such file or directory: 'foo'
+		'''
+		
+		mock = Mock()
+		args = Mock()
+		testBaseDir = os.path.split( os.path.realpath( __file__ ) )[ 0 ]
+		args.file = os.sep.join( ( testBaseDir, '_fixtures', 'issueSource.md' ) )
+			
+		IssueController.actionCreate( mock, args )
+		
+		self.assertEqual( 0, stdinMock.readlines.call_count, 'stdin.readlines call count' )
+		mock.reportIssueFromText.assert_called_once_with( 'Sample title\n\nSample description.' )
+
+	@patch( '__main__.sys.stdin' )
+	def testActionCreateFromEmptyFile( self, stdinMock ):
+		'''
+		For explaination why I didn't mocked read() method, see testActionCreateFromFile.
+		
+		This test ensures that proper exception is generated, when rovided file is empty.
+		'''
+		
+		mock = Mock()
+		args = Mock()
+		testBaseDir = os.path.split( os.path.realpath( __file__ ) )[ 0 ]
+		args.file = os.sep.join( ( testBaseDir, '_fixtures', 'emptyFile.md' ) )
+	
+		self.assertRaisesRegex( RuntimeError, '^The file is empty. You\'re supposed to provide issue content with stdin\.$', IssueController.actionCreate, mock, args )
+		
--- workflow_ignitor/controller/IssueController.py
+++ workflow_ignitor/controller/IssueController.py
@@ -1,4 +1,6 @@
 
+import sys
+
 from workflow_ignitor.controller.Controller import Controller
 from workflow_ignitor.issue.parser.TextParser import TextParser, MissingContentError
 from workflow_ignitor.issue.IssueIntegration import IssueIntegration
@@ -7,12 +9,46 @@ class IssueController( Controller ):
 	
 	_TextParser = TextParser
 	
+	'''
+	Value that this controller is going to be invoked with from CLI, e.g. for "issues" it's going to be reachable with: "app.py issues".
+	
+	This string is mandatory.
+	'''
+	cliAction = 'issues'
+	
 	def process( self, args ):
 		'''
 		Looks for args, and based on that executes proper logic.
 		'''
+		if args.subAction == 'create':
+			self.actionCreate( args )
+	
+	def actionCreate( self, args ):
+		'''
+		Creates an issue.
 		
-		pass
+		By default issue source is taken from stdin, but you can provide also a file source for it.
+		'''
+		
+		issueText = ''
+		errorPrefix = ''
+		
+		if not args.file:
+			stdInput = sys.stdin.readlines()
+			issueText = ''.join( stdInput )
+			errorPrefix = 'Empty buffer given to stdin'
+		else:
+			with open( args.file, 'r' ) as hFile:
+				issueText = ''.join( hFile.readlines() )
+			errorPrefix = 'The file is empty'
+		
+		issueText = issueText.strip()
+		
+		if not issueText:
+			raise RuntimeError( '{0}. You\'re supposed to provide issue content with stdin.'.format( errorPrefix ) )
+		
+		# Reports the issue.
+		self.reportIssueFromText( issueText )
 	
 	def reportIssueFromText( self, issueText ):
 		'''
@@ -25,6 +61,16 @@ class IssueController( Controller ):
 			self._reportIssue( issue, self.owner.getProject() )
 		except MissingContentError as err:
 			raise ValueError( str( err ) )
+	
+	def _registerCommands( self, argParser ):
+		
+		if not isinstance( self.owner.lang, dict ):
+			return 
+		
+		cliLang = self.owner.lang[ 'app' ][ 'issues' ][ 'cli' ]
+		
+		argParser.add_argument( 'subAction', help = cliLang[ 'issuesSubAction' ], choices = [ 'create', 'close' ] )
+		argParser.add_argument( '--file', help = cliLang[ 'file' ], metavar = 'srcFile' )
 	
 	def _reportIssue( self, issue, project ):
 		'''
